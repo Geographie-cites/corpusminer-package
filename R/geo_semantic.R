5d4ca3ac572bba95a7ab46e600bb200cc969c5fa
Romain Francois <romain@purrple.cat>
2017-02-17 20:36:00 +0100

    function aggregateCountriesBasedOnTerms

--- R/geo_semantic.R
+++ R/geo_semantic.R
@@ -10,6 +10,7 @@
 #' @param numberOfGroups an integer giving the number of classes for the clustering
 #' @param themes the list of themes of the analysis
 #' @param groups_Country a vector of group IDs for each country
+#' 
 #' @importFrom stats dist hclust
 #' @export
 cahCountriesBasedOnTerms = function(themes_By_country_bf, numberOfGroups, themes){
@@ -19,4 +20,35 @@ cahCountriesBasedOnTerms = function(themes_By_country_bf, numberOfGroups, themes
   cah.themes <- hclust(d.themes, method = "ward.D2")
   groups_Country <- cutree(cah.themes, k=numberOfGroups)
   groups_Country
+}
+
+#' Aggregate countries based on terms
+#' 
+#' This function summarises the number of articles by theme for each country it is used 
+#' in the reactive object 'clusterCountries' for every analysis at the country level
+#' 
+#' @param themesFile a dataframe in which lines represent articles and columns include themes and country codes
+#' @param themes the list of themes of the analysis
+#' @param countries_to_aggregate the list of countries to aggregate articles by (taken from the shapeFile)
+#' 
+#' @return a dataframe in which lines represent country codes and columns represent the number of articles for each theme
+#' @export
+aggregateCountriesBasedOnTerms = function(themesFile, themes, countries_to_aggregate){
+  themes_By_country_bf <- data.frame("CountryID" = countries_to_aggregate)
+  themes_By_country_bf[,themes] <- NA
+  themes_By_country_bf$n <- NA
+  
+  for (c in countries_to_aggregate){
+    articles_to_aggregate <- themesFile[themesFile[,c] == 1,themes]
+    if (!is.null(articles_to_aggregate)){
+      nArticles <- dim(articles_to_aggregate)[1]
+      themes_By_country_bf[themes_By_country_bf$CountryID == c, themes] <- colSums(articles_to_aggregate) / nArticles
+      themes_By_country_bf[themes_By_country_bf$CountryID == c, "n"] <- nArticles
+    }
+  }
+  
+  themes_By_country_bf <- themes_By_country_bf[complete.cases(themes_By_country_bf),]
+  themes_By_country_bf$CountryID <- substr(themes_By_country_bf$CountryID, 3,4)
+  
+  themes_By_country_bf
 }
