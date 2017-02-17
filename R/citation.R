c83e46a19572dbbfd3a7fd64d22926a5d0f9cf85
Romain Francois <romain@purrple.cat>
2017-02-17 21:22:15 +0100

    minor updates & redocument

--- R/citation.R
+++ R/citation.R
@@ -37,54 +37,52 @@ citationLoadKeywords <- function(con_citation, con_keywords, id){
   keywords
 }
 
-
-
-#'
-#' @name citationVisuEgo
-#' @description visualize an ego network given edges
-citationVisuEgo<-function(edges){
+#' visualize an ego network given edges
+#' @importFrom igraph V E head_of tail_of  graph_from_data_frame 
+#' @importMethodsFrom igraph plot
+#' @export
+citationVisuEgo <- function(edges){
   if(!is.null(edges)){
     if(nrow(edges)>0){
-      citsubgraph = graph_from_data_frame(edges,directed=TRUE)
-      #show(citsubgraph)
-      V(citsubgraph)[head_of(citsubgraph,E(citsubgraph))$name]$cyb = E(citsubgraph)$fromcyb
-      V(citsubgraph)[tail_of(citsubgraph,E(citsubgraph))$name]$cyb = E(citsubgraph)$tocyb
-      V(citsubgraph)[head_of(citsubgraph,E(citsubgraph))$name]$title = E(citsubgraph)$fromtitle
-      V(citsubgraph)[tail_of(citsubgraph,E(citsubgraph))$name]$title = E(citsubgraph)$totitle
-      lay=layout_as_tree(citsubgraph,circular=FALSE)
-      lay[lay[,2]==0,2]=-sample.int(length(which(lay[,2]==0)),replace=FALSE)-2
-      #lay[lay[,2]==2,1]= sample.int(10,size=length(which(lay[,2]==2)))-5#((-length(which(lay[,2]==2))/2):(length(which(lay[,2]==2))/2))*5/length(which(lay[,2]==2))
-      lay[lay[,2]==2,1]= sample.int(length(which(lay[,2]==2)))-5
-      lay[lay[,2]==2,2]=4+sample.int(length(which(lay[,2]==2)),replace=FALSE)
-      palette=c("#df691a","#1C6F91")
+      citsubgraph <- graph_from_data_frame(edges,directed=TRUE)
+      V(citsubgraph)[head_of(citsubgraph,E(citsubgraph))$name]$cyb   <- E(citsubgraph)$fromcyb
+      V(citsubgraph)[tail_of(citsubgraph,E(citsubgraph))$name]$cyb   <- E(citsubgraph)$tocyb
+      V(citsubgraph)[head_of(citsubgraph,E(citsubgraph))$name]$title <- E(citsubgraph)$fromtitle
+      V(citsubgraph)[tail_of(citsubgraph,E(citsubgraph))$name]$title <- E(citsubgraph)$totitle
+      lay <- layout_as_tree(citsubgraph,circular=FALSE)
+      lay[lay[,2]==0,2]  <- -sample.int(length(which(lay[,2]==0)),replace=FALSE)-2
+      #lay[lay[,2]==2,1] <-  sample.int(10,size=length(which(lay[,2]==2)))-5#((-length(which(lay[,2]==2))/2):(length(which(lay[,2]==2))/2))*5/length(which(lay[,2]==2))
+      lay[lay[,2]==2,1]  <-  sample.int(length(which(lay[,2]==2)))-5
+      lay[lay[,2]==2,2]  <- 4+sample.int(length(which(lay[,2]==2)),replace=FALSE)
+      palette <- c("#df691a","#1C6F91")
       par(bg = "#4e5d6c")
-      plot(citsubgraph,edge.color="#df691a",edge.arrow.size = 1,
-           vertex.label=V(citsubgraph)$title,vertex.color=palette[V(citsubgraph)$cyb+1],
-           vertex.frame.color="#1C6F91",vertex.label.color = "#ebebeb",
+      plot(citsubgraph,
+           edge.color="#df691a",
+           edge.arrow.size = 1,
+           vertex.label=V(citsubgraph)$title,
+           vertex.color=palette[V(citsubgraph)$cyb+1],
+           vertex.frame.color="#1C6F91",
+           vertex.label.color = "#ebebeb",
            layout=lay
       )
-      #16283 22232 23337 23502 26325 24841 26026 22270 24798 25354 26969
     }
   }
 }
 
-
-
-
-
-#'
-#' @name citationWordclouds
-#' @description plots word clouds, one for the keywords of the ref itself, the other for the provided keywords (neighborhood)
-citationWordclouds<-function(id,keywords){
-  if(id!="0"&!is.null(keywords)){
+#' plots word clouds, one for the keywords of the ref itself, the other for the provided keywords (neighborhood)
+#' 
+#' @importFrom graphics par
+#' @importFrom wordcloud wordcloud
+citationWordclouds<-function(id, keywords){
+  if(id != "0" && !is.null(keywords)){
     # at least kws for the paper, so no need to check emptyness
-    par(mfrow=c(1,2))
-    par(bg = "#4e5d6c")
+    par(mfrow=c(1,2), bg = "#4e5d6c")
     wordcloud(words=keywords[[id]],
               freq=citationkwfreqs[keywords[[id]]],
               colors=unlist(semanticcolors[citationkwthemdico[keywords[[id]]]]),
               ordered.colors = TRUE
     )
+    
     allkws=unlist(keywords)
     wordcloud(words=allkws,
               freq=citationkwfreqs[allkws],
